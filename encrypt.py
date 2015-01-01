4074e7c9e1c08cf4a108b5473927efe980758fe7
sarath <sarath.sp06@gmail.com>
2015-01-01 13:58:49 +0530

    partial python code..

--- encrypt.py
+++ encrypt.py
@@ -8,12 +8,10 @@ project_home = os.getcwd()
 slicing_constant = 0x100
 def readImage(filename):
     images_home = project_home + '/img/'
-    img = (np.asarray(Image.open(images_home + readImage))[:,:,0]).flatten()
+    img = (np.asarray(Image.open(images_home + filename))[:,:,0]).flatten()
     slicing_diff = img.size // slicing_constant 
-    slicing_start = 
+    slicing_start = slicing_diff // 2
+    return [(i,j) for i, j in enumerate(xrange(slicing_start,img.size,slicing_diff))]
     
-    
-    
-   
     
 readImage('workhard.jpg')
